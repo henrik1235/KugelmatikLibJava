9791d1d465752a4644569d5f0ba18e4c11a4a4ca
Henrik Kunzelmann <hkunzelmann@web.de>
2017-05-09 20:30:50 +0200

    Version 22

--- src/de/karlkuebelschule/KugelmatikLibrary/Kugelmatik.java
+++ src/de/karlkuebelschule/KugelmatikLibrary/Kugelmatik.java
@@ -6,7 +6,7 @@ import java.net.SocketException;
  * Repräsentiert eine Kugelmatik
  */
 public class Kugelmatik {
-    public static final int VERSION = 21;
+    public static final int VERSION = 22;
 
     private Cluster[] clusters;
     private Log log;
